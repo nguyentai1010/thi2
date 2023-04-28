bcf31858d9491b7cbbfd76e273b0ff58eaabace7
nguyentai1010 <111493850+nguyentai1010@users.noreply.github.com>
2023-04-28 15:01:28 +0700

    tai
    tai

---
+++ thi/Application.java
@@ -0,0 +1,7 @@
+package thi;
+public class Application {
+    public static void main(String[] args) {
+        StudentManagementProgram test = new StudentManagementProgram();
+        test.run();
+    }
+}
